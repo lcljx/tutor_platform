7a9b0f460dbd6a32298045fdcb4ad4d3bf8065b5
relys Lin <33930068+lcljx@users.noreply.github.com>
2019-01-16 23:10:25 +0800

    pages

--- src/main/java/com/ljx/tutor_platform/service/impl/MessageValidate.java
+++ src/main/java/com/ljx/tutor_platform/service/impl/MessageValidate.java
@@ -1,41 +1,42 @@
 package com.ljx.tutor_platform.service.impl;
 
 import java.io.IOException;
+import java.util.Random;
 
 import org.json.JSONException;
 
-import com.github.qcloudsms.SmsSingleSender;
-import com.github.qcloudsms.SmsSingleSenderResult;
+import com.github.qcloudsms.SmsMultiSender;
+import com.github.qcloudsms.SmsMultiSenderResult;
 import com.github.qcloudsms.httpclient.HTTPException;
 
 public class MessageValidate {
-	public static void main(String[] args) {
-		// 短信应用SDK AppID
-        int appid = 1400180887; // 1400开头
+	// 短信应用SDK AppID
+	private static int appid = 1400180887; // 1400开头
 
-        // 短信应用SDK AppKey
-        String appkey = "7f16cf9087402e937319dceeacd718f4";
+	// 短信应用SDK AppKey
+	private static String appkey = "7f16cf9087402e937319dceeacd718f4";
 
-        // 需要发送短信的手机号码
-        String[] phoneNumbers = {"18750718852"};
+    // 短信模板ID，需要在短信应用中申请
+    // NOTE: 这里的模板ID`7839`只是一个示例，
+    // 真实的模板ID需要在短信控制台中申请
+    private static int templateId = 267297;
 
-        // 短信模板ID，需要在短信应用中申请
-        // NOTE: 这里的模板ID`7839`只是一个示例，
-        // 真实的模板ID需要在短信控制台中申请
-        int templateId = 267297;
-
-        // 签名
-        // NOTE: 这里的签名"腾讯云"只是一个示例，
-        // 真实的签名需要在短信控制台中申请，另外
-        // 签名参数使用的是`签名内容`，而不是`签名ID`
-        String smsSign = "";
-
-        // 单发短信
+    // 签名
+    // NOTE: 这里的签名"腾讯云"只是一个示例，
+    // 真实的签名需要在短信控制台中申请，另外
+    // 签名参数使用的是`签名内容`，而不是`签名ID`
+    private static String smsSign = "";
+	
+	public String sendMsg(String[] phoneNumbers) {
+		SmsMultiSenderResult result = null;
+		// 需要发送短信的手机号码:phoneNumbers
+		// 指定模板ID单发短信
         try {
-            SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
-            SmsSingleSenderResult result = ssender.send(0, "86", phoneNumbers[0],
-                "【腾讯云】您的验证码是: 5678", "", "");
-            System.out.print(result);
+        	String code = getCode();
+            String[] params = {"5678"};
+            SmsMultiSender msender = new SmsMultiSender(appid, appkey);
+            result =  msender.sendWithParam("86", phoneNumbers,
+                templateId, params, smsSign, "", "");  // 签名参数未提供或者为空时，会使用默认签名发送短信
         } catch (HTTPException e) {
             // HTTP响应码错误
             e.printStackTrace();
@@ -46,5 +47,15 @@ public class MessageValidate {
             // 网络IO错误
             e.printStackTrace();
         }
+		return result.toString();
+	}
+	
+	public String getCode() {
+		StringBuffer sb = new StringBuffer();
+		Random r = new Random();
+		for(int i=0;i<6;i++) {
+			sb.append(r.nextInt(10));
+		}
+		return sb.toString();
 	}
 }
