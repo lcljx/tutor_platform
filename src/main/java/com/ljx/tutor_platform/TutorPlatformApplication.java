bb0ea88c6415cabe530559928256e4797d7ac3ae
relys Lin <33930068+lcljx@users.noreply.github.com>
2019-01-23 14:08:49 +0800

    register validate

--- src/main/java/com/ljx/tutor_platform/TutorPlatformApplication.java
+++ src/main/java/com/ljx/tutor_platform/TutorPlatformApplication.java
@@ -1,6 +1,5 @@
 package com.ljx.tutor_platform;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ljx/tutor_platform/controller/UserController.java
+++ src/main/java/com/ljx/tutor_platform/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController{
         JSONObject json = (JSONObject)request.getSession().getAttribute("code");
         try {
         	String code =  json.getString("code");
-        	System.out.println("code");
+        	System.out.println(code);
 			if(!json.getString("code").equals(user.getVcode())){
 			    return "验证码错误";
 			}else if((System.currentTimeMillis() - json.getLong("createTime")) > 1000 * 60 * 5){
