ba5732a97fdc55cd20573f00b369a85831131390
relys Lin <33930068+lcljx@users.noreply.github.com>
2020-10-25 19:18:02 +0800

    测试网路

--- src/main/java/com/ljx/tutor_platform/TutorPlatformApplication.java
+++ src/main/java/com/ljx/tutor_platform/TutorPlatformApplication.java
@@ -1,5 +1,5 @@
 package com.ljx.tutor_platform;
-
+//
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
